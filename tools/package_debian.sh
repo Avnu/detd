5e23e72b2b9443d2cf072d2fe7032300b99435eb
xtor <hector.blanco.alcaine@intel.com>
2022-06-11 00:02:20 +0100

    Add script to generate a deb package

---
+++ tools/package_debian.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(C) 2022 Intel Corporation
+# Authors:
+#   Hector Blanco Alcaine
+#
+# ./package_debian.sh
+#
+# Generates a rudimentary deb package to facilitate installations on Debian
+# based distributions. The package is then copied to /tmp
+
+
+
+
+function usage () {
+   echo "Usage:"
+   echo "$0"
+}
+
+
+
+function parse_args () {
+
+   # Check the number of arguments supplied
+   if [ $# -gt 0  ]; then
+      echo "Wrong number of arguments"
+      usage
+      exit 1
+   fi
+
+}
+
+
+function check_args () {
+   # Placeholder
+   :
+}
+
+
+function create_deb () {
+
+	cd ..
+
+
+	# Create the source distribution
+	python3 setup.py sdist
+
+
+	# Replicate the id generated for the source distribution tarball
+	# E.g. detd-0.1.dev0 (PKG: detd, VERSION: 0.1.dev0)
+	PKG=$(awk '/^name/{print $3}' ./setup.cfg)
+	VERSION=$(awk '/^version/{print $3}' ./setup.cfg)
+	ID="${PKG}-${VERSION}"
+
+
+	# Create a tempdir and extract the package there
+	TMPDIR=`mktemp -d --suffix=.detd`
+	cp dist/${ID}.tar.gz ${TMPDIR}
+	tar xvzf ${TMPDIR}/${ID}.tar.gz -C ${TMPDIR}
+
+
+	# Generate and customize the debian directory
+	cd ${TMPDIR}/${ID}
+	debmake --binaryspec ':py3' --email ${EMAIL} --fullname ${FULLNAME} --spec
+
+	sed -i 's/^Section:[^$]*$/Section: Networking/' debian/control
+	sed -i 's/^Homepage:[^$]*$/Homepage\: https\:\/\/github.com\/Avnu\/detd/' debian/control
+	sed -i 's/^X-Python3-Version:[^$]*$/X-Python3-Version: >= 3.8/' debian/control
+	sed -i 's/^Build-Depends:\([^,]*\),$/Build-Depends: protobuf-compiler,\n              \1,/' debian/control
+	sed -i 's/^Depends:\(.*\)/Depends: iproute2,\n        \1/' debian/control
+	sed -i 's/^Depends:\(.*\)/Depends: ethtool,\n        \1/' debian/control
+	sed -i '/^Description:.*/i Recommends: cgroup-tools' debian/control
+
+	# Force xz for compression, to prevent installation issues with Zstandard
+	echo -e "\noverride_dh_builddeb:\n\tdh_builddeb -- -Zxz" >> debian/rules
+
+	# Generate the deb, make it available and perform clean-up
+	fakeroot debian/rules binary
+	dpkg -I ../*deb
+	cp ../*deb /tmp
+	echo "The deb package is now available in /tmp"
+
+	rm -rf ${TMPDIR}
+
+}
+
+
+# Customize name and email for the maintainer fields
+EMAIL="foo@bar"
+FULLNAME="Foobar"
+
+parse_args "$@"
+check_args
+
+create_deb
+
+
+exit 0
