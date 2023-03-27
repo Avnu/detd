3f659d92c6906b09a8b662304cd2d46848b78311
jeppex1 <41836686+jeppex1@users.noreply.github.com>
2023-03-27 17:40:05 +0200

    Revert "Added Tests for taprio txassist and etf CommandStrings"
    This reverts commit 97346a76ae7a5d0648d3344b0ad2fc7c86a89b13.

--- tests/test_commandstring.py
+++ tests/test_commandstring.py
@@ -18,8 +18,6 @@ from detd import CommandStringEthtoolSetRing
 from detd import CommandStringEthtoolGetDriverInformation
 from detd import CommandStringTcTaprioOffloadSet
 from detd import CommandStringTcTaprioSoftwareSet
-from detd import CommandStringTcTaprioTxassistSet
-from detd import CommandStringTcEtfInstall
 
 
 
@@ -204,62 +202,6 @@ class TestCommandString(unittest.TestCase):
                     sched-entry S 01 19737824
                     flags     0x0
                     clockid CLOCK_TAI'''
-
-
-        self.assert_commandstring_equal(cmd, expected)
-
-
-    def test_tcsettapriotxassist(self):
-
-        interface_name = "eth0"
-        soprio_to_tc   = "0 0 0 0 0 0 1 0 0 0 0 0"
-        num_tc         = 2
-        tc_to_hwq      = "1@0 1@1 1@2 1@3 1@4 1@5 1@6 1@7"
-        base_time      = "165858970408520000"
-        sched_entries  = """sched-entry S 01 250000
-                            sched-entry S 02 12176
-                            sched-entry S 01 19737824"""
-        handle         = "100"
-        txtime_delay   = "500000"
-
-        cmd = CommandStringTcTaprioTxassistSet(interface_name, num_tc, soprio_to_tc, tc_to_hwq, base_time, sched_entries, handle, txtime_delay)
-        expected = '''
-           tc qdisc replace
-                    dev       eth0
-                    parent    root
-                    handle    100
-                    taprio
-                    num_tc    2
-                    map       0 0 0 0 0 0 1 0 0 0 0 0
-                    queues    1@0 1@1 1@2 1@3 1@4 1@5 1@6 1@7
-                    base-time 165858970408520000
-                    sched-entry S 01 250000
-                    sched-entry S 02 12176
-                    sched-entry S 01 19737824
-                    flags     0x1
-                    txtime-delay 500000
-                    clockid CLOCK_TAI'''
-
-
-        self.assert_commandstring_equal(cmd, expected)
-
-
-    def test_tcetfinstall(self):
-
-        interface_name = "eth0"
-        parent         = "100:1"
-        delta          = "500000"
-
-        cmd = CommandStringTcEtfInstall(interface_name, parent, delta)
-        expected = '''
-           tc qdisc replace
-                    dev       eth0
-                    parent    100:1
-                    etf
-                    clockid CLOCK_TAI
-                    delta 500000
-                    offload
-                    skip_sock_check'''
 
 
         self.assert_commandstring_equal(cmd, expected)
