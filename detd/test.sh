8f8545950d8df264facfc8f78073cd1b4a873960
xtor <hector.blanco.alcaine@intel.com>
2024-10-01 15:48:48 +0200

    Generate protobuf code in test script, add header
    Signed-off-by: xtor <hector.blanco.alcaine@intel.com>

--- detd/test.sh
+++ detd/test.sh
@@ -1,3 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(C) 2022 Intel Corporation
+# Authors:
+#   Hector Blanco Alcaine
+#
+# ./test.sh
+#
+# Generates the protobuf code based in the ipc.proto declaration, runs the unit
+# tests and deletes the generated code.
 
+protoc ipc.proto --python_out=.
 python3 -m unittest discover .. --verbose
+rm ipc_pb2.py
