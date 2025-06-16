fbda13778e89016ecaad7b768c68c43c5ba0b1c7
xtor <hector.blanco.alcaine@intel.com>
2025-06-16 09:11:40 +0200

    Add executing unit tests based on a pattern

--- detd/test.sh
+++ detd/test.sh
@@ -4,21 +4,23 @@
 # Authors:
 #   Hector Blanco Alcaine
 #
-# ./test.sh
+# ./test.sh [PATTERN]
 #
 # Generates the protobuf code based in the ipc.proto declaration, runs the unit
 # tests and deletes the generated code.
 
-protoc ipc.proto --python_out=.
-if [ "$#" -eq 0 ]; then
-   python3 -m unittest discover .. --verbose
-elif [ "$#" -eq 1 ]; then
-   cd ..
-   python3 -m unittest "$1" --verbose
-   cd -
-else
+if [ "$#" -gt 1 ]; then
    echo "Usage: $0 [unit test to execute]"
+   exit 0
 fi
+
+PATTERN=""
+if [ "$#" -eq 1 ]; then
+   PATTERN="-k $1"
+fi
+
+protoc ipc.proto --python_out=.
+python3 -m unittest discover .. --verbose ${PATTERN}
 rm ipc_pb2.py
 
-echo -e "\nUnit testing logs available in detd-server-unittest.log"
+echo -e "\nUnit testing logs available in detd-unittest.log"
